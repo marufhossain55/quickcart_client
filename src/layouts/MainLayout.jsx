1e39249f4ede047cb536c1d939e385767624d509
marufhossain55 <engrmaruf3@gmail.com>
2024-11-20 16:07:22 +0600

    navbar footer added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,14 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Footer from '../components/home/Footer';
+
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 export default MainLayout;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
+import Register from '../pages/authentication/Register';
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
+    children: [{ path: '/register', element: <Register /> }],
   },
 ]);
